d9b7491cb303531d9a6f8f685dbe3843ec407ce1
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-05 20:15:04 +0800

    zuoye

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,15 @@
 //TODO
+package com.example.SpringIoCpractice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	
+    public static void main(String[] args) {
+		
+	  ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"hello.xml"});
+        HelloWorld HelloWorld = (HelloWorld) context.getBean("helloWorld");
+        HelloWorld.getHello();
+        }
+}
